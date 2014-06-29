2ff525ba6a077eaa843d6065fd97a376654a546e
thrashr888 <thrashr888@gmail.com>
2014-06-29 16:59:13 -0700

    Use public api instead of local proxy.

--- app/scripts/config.jsx
+++ app/scripts/config.jsx
@@ -5,8 +5,8 @@
 'use strict';
 
 var Config = {
-    // marvelApiEndpoint: 'http://gateway.marvel.com',
-    marvelApiEndpoint: 'http://localhost:9001',
+    marvelApiEndpoint: 'http://gateway.marvel.com',
+    // marvelApiEndpoint: 'http://localhost:9001',
     marvelUserKey: 'ae5b76f41876fbf3e9c8985ea1e9c807'
 };
 
